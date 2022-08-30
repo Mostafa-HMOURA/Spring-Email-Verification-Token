43f2e9c0918fc086cd841a57c6b69013e829f8a4
Mostafa-HMOURA <mostafahmoura@gmail.com>
2022-08-30 10:31:44 +0100

    final commit

---
+++ src/main/java/com/mostafa/mailsender/MostafaApplication.java
@@ -0,0 +1,13 @@
+package com.mostafa.mailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostafaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MostafaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mostafa/mailsender/MostafaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mostafa.mailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MostafaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
